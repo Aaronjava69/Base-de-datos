6b58fc69db3d6c8e0fa2e2f9057d14d143798106
Aaronjava69 <aaronhuamaniconopuma@gmail.com>
2024-01-31 23:08:06 +0100

    Comics

---
+++ Definicion.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE Ejemplo1; #crear una base de datos nueva
+
+USE Ejemplo1 #usa la tabla 
+
+CREATE TABLE Ejemplo1 #creamos la tabla, y entre parentesis crearemos las columnas
+
+SHOW Ejemplo1 #Mostrar base de datos y tablas
+
+INSERT INTO ejemplo1 VALUE #Insertar valores dentro de la tabla
+
+SELECT * FROM ejemplo1
+
+#Se pone debajo de una foreign key
+ON DELETE CASCADE
+ON UPDATE CASCADE 
